a3c577f218f21940fb90002054ed065657cc27ae
kevin-peng <kevin.peng02@sap.com>
2023-11-25 20:24:34 -0800

    add search endpoint

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # semantic-search
+
+
+## API
+### Activate venv
+`source semantic-search-venv/bin/activate`
+
+### Create venv and install deps
+`python3 -m venv {venv-name}`
+
+`pip3 install -r requirements.txt`
+
+### Save deps
+`pip3 freeze > requirements.txt`
+
+### Start localhost
+`./api/manage.py runserver`
--- api/api/api.py
+++ api/api/api.py
@@ -1,11 +1,31 @@
 from ninja import NinjaAPI, Schema
+import cohere
+import numpy as np
+import os
+
+cohere_key = os.getenv("COHERE_API_KEY")
+cohere_model = os.getenv("COHERE_MODEL")
+
+co = cohere.Client(cohere_key)
 
 api = NinjaAPI()
 
-class MatchesSchema(Schema):
+class SearchSchema(Schema):
     query: str
-    text: str
+    texts: list[str]
 
-@api.get("/matches")
-def matches(request, data: MatchesSchema):
-    return data
+
+@api.get("/search")
+def search(request, data: SearchSchema):
+    #Encode your documents with input type 'search_document'
+    doc_emb = co.embed(data.texts, input_type="search_document", model=cohere_model).embeddings
+    doc_emb = np.asarray(doc_emb)
+
+    #Encode your query with input type 'search_query'
+    query_emb = co.embed([data.query], input_type="search_query", model=cohere_model).embeddings
+    query_emb = np.asarray(query_emb)
+
+    #Compute the dot product between query embedding and document embedding
+    scores = np.dot(query_emb, doc_emb.T)[0]
+
+    return scores.tolist()
